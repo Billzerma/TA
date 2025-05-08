abe0fe827aa22a05840d4a765788e340bcbcaad7
Billzerma <nabilzerma272002@gmail.com>
2025-05-08 17:46:55 +0700

    yok bisa yok

---
+++ luminanapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),  
+    path('signup/', views.signup_view, name='signup'), 
+    path('tentang/', views.tentang_view, name='tentang'), 
+    path('galeri/', views.galeri_view, name='galeri'),
+]
---
+++ luminanapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'luminance/home.html')
+
+def login_view(request):
+    return render(request, 'luminance/login.html')
+
+def signup_view(request):
+    return render(request, 'luminance/signup.html')
+
+def tentang_view(request):
+    return render(request, 'luminance/tentang.html')
+
+def galeri_view(request):
+    return render(request, 'luminance/galeri.html')
